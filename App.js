ce6288a632cec12efda349996e3bebb3b5896779
z00m-in <81089850+z00m-in@users.noreply.github.com>
2021-03-30 19:02:45 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./comonents/Subject";
+import './App.css';
+
+
+
+
+
+
+class App extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    subject:{title='WEB', sub='world wide web!'},
+    contents: [
+      {id:1, title:'HTML', desc: 'HTML is for information'}, 
+      {id:2, title:'CSS', desc: 'CSS is for design'}, 
+      {id:3, title:'JavaScript', desc: 'JavaScript is for interactive'}
+    ]
+      
+  }
+
+}
+
+  render () {
+    return (
+      <div className="App">
+       <Subject
+        title={this.state.subject.title}
+        sub={this.state.subject.sub}>
+       </Subject>
+       <TOC></TOC>
+       <Content title='HTML' desc='HTML is HyperText Markup Language.'></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
