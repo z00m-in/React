00d86a022d10bc74a398fd33f42e35a797a56eb3
z00m-in <81089850+z00m-in@users.noreply.github.com>
2021-04-06 16:55:44 +0900

    Add files via upload

--- Subject.js
+++ Subject.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-
 class Subject extends Component {
-    render(){
-      return (
-        <header>
-              <h1>{this.props.title}</h1>
-              {this.props.sub}
-          </header>
-      );
+    render() {
+        console.log('Subject render');
+        return (
+         <header>
+             <h1><a href="/" onClick={function(e) {
+                 e.preventDefault();
+                 this.props.onChangePage();
+             }.bind(this)}>{this.props.title}</a></h1>
+             {this.props.sub}
+         </header>
+        );
     }
-  }
+}
 
-  export default Subject;
+export default Subject;
